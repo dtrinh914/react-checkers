a374aaefc637ad5c4617b02c97db7ae8e5d429ac
dtrinh914 <dtrinh914@gmail.com>
2020-04-27 13:10:40 -0700

    Updates

---
+++ precache-manifest.e63f139271f73a88370942e368c1669c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb3358b1eaad7b61c9e85a7d7b7e83d",
+    "url": "/react-checkers/index.html"
+  },
+  {
+    "revision": "5cf4076cc36f73855de1",
+    "url": "/react-checkers/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "20ca40d1eedcde22b14c",
+    "url": "/react-checkers/static/js/2.504986a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-checkers/static/js/2.504986a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf4076cc36f73855de1",
+    "url": "/react-checkers/static/js/main.a4ff9281.chunk.js"
+  },
+  {
+    "revision": "ac2e5cd89af0db97f48d",
+    "url": "/react-checkers/static/js/runtime-main.bc68a9e9.js"
+  }
+]);
