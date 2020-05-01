bdaabca8788866b5d57c4db829bf599ce5ea1314
dtrinh914 <dtrinh914@gmail.com>
2020-05-01 09:29:20 -0700

    fix view height on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import Board from './Board';
 import MultiBackend from 'react-dnd-multi-backend';
 import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch'; 
@@ -8,11 +8,19 @@ const appStyle : React.CSSProperties = {
   display: 'flex',
   justifyContent: 'center',
   alignItems: 'center',
-  height: '100vh',
+  height: 'var(--app-height)',
   width: '100vw'
 }
 
 const App : React.FC = () => {
+  useEffect(()=>{
+    const appHeight = () => document.documentElement.style.setProperty('--app-height', `${window.innerHeight}px`);
+    window.addEventListener('resize', appHeight);
+    appHeight();
+
+    return () => window.removeEventListener('resize', appHeight);
+  }, []);
+
   return (
     <div style={appStyle}>
       <DndProvider backend={MultiBackend} options={HTML5toTouch}>
